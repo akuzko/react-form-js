a4729dffffa100cd7933adebe74fe5427ce13604
Oksana Kuzko <ksy.grechishkina@gmail.com>
2017-02-12 21:46:23 +0200

    style updates for demo app
    - use GHColors theme for Prism
    - update CSS and markup for demo app

--- demo/src/DemoForm.jsx
+++ demo/src/DemoForm.jsx
@@ -17,10 +17,10 @@ export default class DemoForm extends Form {
   $render($) {
     return (
       <div className="container">
-        <div className="paper p-20 mt-20 mb-20">
+        <div className="mt-20 mb-20">
           <div className="horizontal-container">
-            <div className="flex-item three mr-20">
-              <div className="horizontal-container mb-20">
+            <div className="flex-item mr-20">
+              <div className="paper horizontal-container center p-20 mb-20">
                 <div className="flex-item two mr-20">
                   <Source code={`
                     <TextField
@@ -33,12 +33,12 @@ export default class DemoForm extends Form {
                   `} />
                 </div>
                 <div className="flex-item">
-                  <TextField {...$('fullName')} placeholder="Full Name" label="TextField" className="block mb-10" inputClassName="form-control" />
-                  <button onClick={this.makeInvalid.bind(this, 'fullName')}>Validate</button>
+                  <TextField {...$('fullName')} placeholder="Full Name" label="TextField" className="block mb-20" inputClassName="form-control" />
+                  <button className="btn green" onClick={this.makeInvalid.bind(this, 'fullName')}>Validate</button>
                 </div>
               </div>
 
-              <div className="horizontal-container mb-20">
+              <div className="paper horizontal-container center p-20 mb-20">
                 <div className="flex-item two mr-20">
                   <Source code={`
                     <TextArea
@@ -51,13 +51,13 @@ export default class DemoForm extends Form {
                   `} />
                 </div>
                 <div className="flex-item">
-                  <TextArea {...$('description')} placeholder="Description" label="TextArea" className="block" inputClassName="form-control" />
-                  <button onClick={this.makeInvalid.bind(this, 'description')}>Validate</button>
+                  <TextArea {...$('description')} placeholder="Description" label="TextArea" className="block mb-20" inputClassName="form-control" />
+                  <button className="btn green" onClick={this.makeInvalid.bind(this, 'description')}>Validate</button>
                 </div>
               </div>
 
 
-              <div className="horizontal-container mb-20">
+              <div className="paper horizontal-container center p-20 mb-20">
                 <div className="flex-item two mr-20">
                   <Source code={`
                     <Select
@@ -71,24 +71,24 @@ export default class DemoForm extends Form {
                   `} />
                 </div>
                 <div className="flex-item">
-                  <Select {...$('position')} options={[1, 2, 3]} includeBlank="Position..." label="Select" className="block" inputClassName="form-control" />
-                  <button onClick={this.makeInvalid.bind(this, 'position')}>Validate</button>
+                  <Select {...$('position')} options={[1, 2, 3]} includeBlank="Position..." label="Select" className="block mb-20" inputClassName="form-control" />
+                  <button className="btn green" onClick={this.makeInvalid.bind(this, 'position')}>Validate</button>
                 </div>
               </div>
 
-              <div className="horizontal-container mb-20">
+              <div className="paper horizontal-container center p-20 mb-20">
                 <div className="flex-item two mr-20">
                   <Source code={`
                     <Checkbox {...$('checkbox')} label={<span>Checkbox</span>} className="block checkbox" />
                   `} />
                 </div>
                 <div className="flex-item">
-                  <Checkbox {...$('checkbox')} label={<span>Checkbox</span>} className="block checkbox" />
-                  <button onClick={this.makeInvalid.bind(this, 'checkbox')}>Validate</button>
+                  <Checkbox {...$('checkbox')} label={<span>Checkbox</span>} className="block checkbox mb-20" />
+                  <button className="btn green" onClick={this.makeInvalid.bind(this, 'checkbox')}>Validate</button>
                 </div>
               </div>
 
-              <div className="horizontal-container mb-20">
+              <div className="paper horizontal-container center p-20">
                 <div className="flex-item two mr-20">
                   <Source code={`
                     <RadioButtonGroup className="horizontal-container" {...$('radio')}>
@@ -100,20 +100,22 @@ export default class DemoForm extends Form {
                   `} />
                 </div>
                 <div className="flex-item">
-                  <RadioButtonGroup className="horizontal-container" {...$('radio')}>
-                    <div>Favorite Color</div>
+                  <div className="mb-10">Favorite Color:</div>
+                  <RadioButtonGroup className="horizontal-container relative radio-group mb-20" {...$('radio')}>
                     <RadioButton className="radio mr-20" value="red" label={<span>Red</span>} />
                     <RadioButton className="radio mr-20" value="green" label={<span>Green</span>} />
                     <RadioButton className="radio" value="blue" label={<span>Blue</span>} />
                   </RadioButtonGroup>
-                  <button onClick={this.makeInvalid.bind(this, 'radio')}>Validate</button>
+                  <button className="btn green" onClick={this.makeInvalid.bind(this, 'radio')}>Validate</button>
                 </div>
               </div>
             </div>
 
-            <div className="flex-item">
-              <pre>attrs: {JSON.stringify(this.props.attrs, null, '  ')}</pre>
-              <pre>errors: {JSON.stringify(this.state.errors, null, '  ')}</pre>
+            <div className="attrs">
+              <div className="attrs-content">
+                <pre>attrs: {JSON.stringify(this.props.attrs, null, '  ')}</pre>
+                <pre>errors: {JSON.stringify(this.state.errors, null, '  ')}</pre>
+              </div>
             </div>
           </div>
         </div>
